933d1218420b83731620bf3e49fac9d59d8d06d0
tebinhas <mateus_tebas02@outlook.com>
2025-04-23 19:24:38 +0000

    adciionado o scanf para cada variavel

--- SuperTrunfo_Iniciante.c
+++ SuperTrunfo_Iniciante.c
@@ -2,12 +2,25 @@
 
 int main(){
     //Titulo
-    printf ("SuperTrunfo Paises");
+    printf ("SuperTrunfo Paises\n");
 
 //Variaveis
 char cidadeA1[20],cidadeA2[20];
-int populacaoA1,populacaoA2;
-int pontosturisticosA1,pontosturisticosA2;
+int populacaoA1,populacaoA2,pontosturisticosA1,pontosturisticosA2;
 float areaA1,areaA2;
+
+//Declaração das variaveis Cidade A1
+//Nome:
+printf ("Nome da Cidade:\n");
+scanf ("%[^\n]",&cidadeA1);
+//População
+printf ("População:\n");
+scanf ("%d",&populacaoA1);
+//Pontos Turisticos
+printf ("Pontos Turisticos\n");
+scanf("%d",&pontosturisticosA1);
+//Area
+printf ("Area da Cidade\n");
+scanf("%f",&areaA1);
 
 }
