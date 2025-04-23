0bf1073ad22983cbc313e47c6aa0b573ec7aa85b
tebinhas <mateus_tebas02@outlook.com>
2025-04-23 20:07:36 +0000

    declaracao das variaveis segunda carta feita

--- SuperTrunfo_Iniciante.c
+++ SuperTrunfo_Iniciante.c
@@ -5,22 +5,45 @@ int main(){
     printf ("SuperTrunfo Paises\n");
 
 //Variaveis
-char cidadeA1[20],cidadeA2[20];
-int populacaoA1,populacaoA2,pontosturisticosA1,pontosturisticosA2;
-float areaA1,areaA2;
+char cidadeA1[20],cidadeB1[20];
+int populacaoA1,populacaoB1,pontosturisticosA1,pontosturisticosB1;
+float areaA1,areaB1,pibA1,pibB1;
 
-//Declaração das variaveis Cidade A1
+//Declaração das Variaveis Primeira Carta
+printf ("Dados da Primeira Carta:\n");
 //Nome:
 printf ("Nome da Cidade:\n");
-scanf ("%[^\n]",&cidadeA1);
+scanf (" %[^\n]",&cidadeA1);
 //População
 printf ("População:\n");
 scanf ("%d",&populacaoA1);
 //Pontos Turisticos
-printf ("Pontos Turisticos\n");
+printf ("Pontos Turisticos:\n");
 scanf("%d",&pontosturisticosA1);
 //Area
-printf ("Area da Cidade\n");
+printf ("Area da Cidade:\n");
 scanf("%f",&areaA1);
+//PIB
+printf ("PIB da Cidade:\n");
+scanf("%f",&pibA1);
+
+//Declaração das Variaveis Segunda Carta
+printf ("Dados da Segunda Carta:\n");
+//Nome:
+printf ("Nome da Cidade:\n");
+scanf (" %[^\n]",&cidadeB1);
+//População
+printf ("População:\n");
+scanf ("%d",&populacaoB1);
+//Pontos Turisticos
+printf ("Pontos Turisticos\n");
+scanf("%d",&pontosturisticosB1);
+//Area
+printf ("Area da Cidade\n");
+scanf("%f",&areaB1);
+//PIB
+printf ("PIB da Cidade:\n");
+scanf("%f",&pibB1);
+
 
 }
