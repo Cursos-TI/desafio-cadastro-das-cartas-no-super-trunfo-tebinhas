417c0dc46e8d6a6931432eb9f6cc2693c3c98226
tebinhas <mateus_tebas02@outlook.com>
2025-04-23 18:39:44 +0000

    Variaveis definidas: cidade 1 e 2

--- SuperTrunfo_Iniciante.c
+++ SuperTrunfo_Iniciante.c
@@ -4,8 +4,10 @@ int main(){
     //Titulo
     printf ("SuperTrunfo Paises");
 
-
-
-
+//Variaveis
+char cidadeA1[20],cidadeA2[20];
+int populacaoA1,populacaoA2;
+int pontosturisticosA1,pontosturisticosA2;
+float areaA1,areaA2;
 
 }
