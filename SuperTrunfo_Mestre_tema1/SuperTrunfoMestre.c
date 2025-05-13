3171b5e1d8855beff082c752c02a357ea7417a37
tebinhas <mateus_tebas02@outlook.com>
2025-05-13 18:41:31 +0000

    supertrunfo mestre iniciado, atributos atualizados - Populacao agora é unsigned int

---
+++ SuperTrunfo_Mestre_tema1/SuperTrunfoMestre.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+//Atributos
+char cidadeA1[20], cidadeB1[20];
+int pontosturisticosA1, pontosturisticosB1;
+unsigned long int populacaoA1, populacaoB1;
+double pibA1, pibB1;
+float areaA1, areaB1, pibpercaptaA1, pibpercaptaB1, populacaoporareaA1, populacaoporareaB1;
+
+
+
+
+
+
+
+}
